dff723f8cd54070dde83c78c8fe76c859518c965
Preet Panchal <panc0051@algonquinlive.com>
2022-08-08 11:58:30 -0400

    main

---
+++ JS/main.js
@@ -0,0 +1,354 @@
+// In Laravel, @yield is used to do what?
+
+// Question 1 options:
+
+// To stop the current from running
+
+
+// To display the content of a section
+
+
+// To extend a layout
+
+
+// To include a file
+
+//Answer: To display the content of a section
+
+// Question 2 (0.1 points) 
+// In which directory are Models located in a Laravel 8 project?
+
+// Question 2 options:
+
+// /app/Models/
+
+
+// /app/Http/Models/
+
+
+// /resources/Models/
+
+
+// /app/
+
+//Answer: /app/Http/Models/ (wrong)
+//Answer: /app/Models/ (correct)
+
+// Question 3 (0.1 points) 
+// Which command is used to create a new Laravel Model?
+
+// Question 3 options:
+
+// artisan make:model Model_Name
+
+
+// composer model:make Model_Name
+
+
+// php artisan make:model Model_Name
+
+
+// php artisan model:make Model_Name
+
+//Answer: php artisan make:model Model_Name
+
+// Question 4 (0.1 points) 
+// Using Eloquent, which method is used to remove a row from the table?
+
+// Question 4 options:
+
+// delete
+
+
+// remove
+
+
+// dump
+
+
+// unset
+
+//Answer: delete
+
+// Question 5 (0.1 points) 
+// What does the function dd() mean?
+
+// Question 5 options:
+
+// Date and Day
+
+
+// Directory and Door
+
+
+// Dump and Die
+
+
+// Do or Die
+
+//Answer: Dump and Die
+
+// Question 6 (0.1 points) 
+// In which directory are Controllers located in a Laravel 8 project? 
+
+// Question 6 options:
+
+// /resources/Controllers/
+
+
+// /app/Http/Controllers/
+
+
+// /app/Controllers/
+
+
+// /config/Controllers/
+
+//Answer: /app/Http/Controllers/
+
+// Question 7 (0.1 points) 
+// Using Eloquent, what method is used to retrieve every row in a table?
+
+// Question 7 options:
+
+// each
+
+
+// all
+
+
+// every
+
+
+// many
+
+//Answer: all
+
+// Question 8 (0.1 points) 
+// Which file is used to set the database connection in Laravel?
+
+// Question 8 options:
+
+// database.php
+
+
+// config.php
+
+
+// .env
+
+
+// setting.php
+
+// Question 9 (0.1 points) 
+// Which option properly adds an optional route parameter to the URI?
+
+// Question 9 options:
+
+// Route::get('/posts/:post:', function ($post) { ... } );
+
+
+// Route::get('/posts/$post?', function ($post) { ... } );
+
+
+// Route::get('/posts/{?post}', function ($post) { ... } );
+
+
+// Route::get('/posts/{post?}', function ($post) { ... } );
+
+//Answer: Route::get('/posts/{post?}', function ($post) { ... } );
+// Question 10 (0.1 points) 
+// In which directory are the routing files located in a Laravel 8 project? 
+
+
+// Question 10 options:
+
+// /resources/routes/
+
+
+// /routes/
+
+
+// /public/routes/
+
+
+// /app/Http/routes/
+//Answer: /app/Http/routes/ (wrong)
+//Answer: /app/routes/ (correct)
+
+// Question 11 (0.1 points) 
+// Which option properly adds a route parameter to the URI?
+
+// Question 11 options:
+
+// Route::get('/posts/$post', function ($post) { ... } );
+
+
+// Route::get('/posts/{post}', function ($post) { ... } );
+
+
+// Route::get('/posts/:post', function ($post) { ... } );
+
+
+// Route::get('/posts/?post', function ($post) { ... } );
+//Answer: Route::get('/posts/{post}', function ($post) { ... } );
+// Question 12 (0.1 points) 
+// In which directory are Views located in a Laravel 8 project?
+
+// Question 12 options:
+
+// /public/views/
+
+
+// /views/
+
+
+// /app/views/
+
+
+// /resources/views/
+//Answer: /resources/views/
+
+// Question 13 (0.1 points) 
+// Which of the following is NOT true for route in Laravel?
+
+// Question 13 options:
+
+// Arguments can be passed to a route
+
+
+// Routes can point to a method on a controller
+
+
+// A route is an endpoint specified by an URI
+
+
+// Routes can only handle GET requests 
+//Answer: A route is an endpoint specified by an URI (wrong)
+//Answer: Routes can only handle GET requests (correct)
+
+// Question 14 (0.1 points) 
+// Laravel is based on which design pattern?
+
+// Question 14 options:
+
+// MVVM Design Pattern
+
+
+// MVC Design Pattern
+
+
+// Composite Design Pattern
+
+
+// Singleton Design Pattern
+
+//Answer: MVC Design Pattern
+
+// Question 15 (0.1 points) 
+// Using Eloquent, what method is used to add new data to the table?
+
+// Question 15 options:
+
+// store
+
+
+// execute
+
+
+// add
+
+
+// save
+//Answer: store (wrong)
+//Answer: save (correct)
+
+// Question 16 (0.1 points) 
+// Following common conventions, what should the Laravel Model name be for the table posts? 
+
+// Question 16 options:
+
+// Posts
+
+
+// post
+
+
+// Post
+
+
+// posts
+
+//Answer: Posts (wrong)
+//Answer: posts (correct)
+
+// Question 17 (0.1 points) 
+// What kind of HTML code does @csrf generate?
+
+// Question 17 options:
+
+// Label for required field
+
+
+// Submit button
+
+
+// Input field with hidden type
+
+
+// Login form
+//Answer: Input field with hidden type
+
+// Question 18 (0.1 points) 
+// In Laravel, @extends is used to do what?
+
+// Question 18 options:
+
+// Specifies which layout a view should inherit
+
+
+// Defines a section of content
+
+
+// Outputs all the contents of an array or collection
+
+
+// Adds custom functionality to Laravel
+
+//Answer: Specifies which layout a view should inherit
+
+// Question 19 (0.1 points) 
+// What is the name of the Laravel command line interface?
+
+// Question 19 options:
+
+// laravel
+
+
+// composer
+
+
+// artisan
+
+
+// horizon
+
+//Answer: laravel (wrong)
+//Answer: composer (correct)
+// Question 20 (0.1 points) 
+// Which templating engine is included with Laravel?
+
+// Question 20 options:
+
+// Blade
+
+
+// Twig
+
+
+// No templating engine is included
+
+
+// Liquid
+
+//Answer: Blade
