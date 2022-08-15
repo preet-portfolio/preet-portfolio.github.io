aa6961421f80ef01c45726f7834dfeb37fbe71c7
Preet Panchal <panc0051@algonquinlive.com>
2022-08-14 21:05:09 -0400

    main

--- Projects/ReactJS/feedback-app/src/App.js
+++ Projects/ReactJS/feedback-app/src/App.js
@@ -1,30 +1,17 @@
+
+import Header from "./components/Header"
 function App() {
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        { id: 1, text: 'Comment one' },
-        { id: 2, text: 'Comment two' },
-        { id: 3, text: 'Comment three' },
-    ]
 
     return (
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map(comment, index) => (
-                    <li>
-                        {comment.text}
-                    </li>
-                    ))}
-                </ul>
+        <>
+            <Header />
+            <div className='container'>
+                <h1>My App</h1>
 
             </div>
-        </div>
+        </>
     )
 }
+
 
 export default App
